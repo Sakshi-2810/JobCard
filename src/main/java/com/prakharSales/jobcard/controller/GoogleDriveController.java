0147874456466feedef308c9b0b7be63a48b8d5f
Sakshi.Agrawal <sakshi.agrawal@geminisolutions.com>
2025-09-28 16:16:03 +0530

    changes

--- src/main/java/com/prakharSales/jobcard/controller/GoogleDriveController.java
+++ src/main/java/com/prakharSales/jobcard/controller/GoogleDriveController.java
@@ -4,7 +4,6 @@ import com.prakharSales.jobcard.service.GoogleDriveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,7 +18,7 @@ public class GoogleDriveController {
     @PostMapping("/uploadToDrive")
     public Map<String, Object> uploadToDrive(@RequestParam("file") List<MultipartFile> multipartFile) throws Exception {
         List<String> mimeType = multipartFile.stream().map(MultipartFile::getContentType).toList();
-        List<String> files = GoogleDriveService.uploadFileToDrive(multipartFile, mimeType);
+        List<String> files = googleDriveService.uploadFileToDrive(multipartFile, mimeType);
         return Map.of("fileUrl", files);
     }
 }
